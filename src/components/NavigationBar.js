ef793c54922565b384b5b215215a9b0ec559484a
Pranav-Rustagi <pranav001100@gmail.com>
2023-03-26 17:28:24 +0530

    minor changes

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem } from "reactstrap";
 
 const NavigationBar = () => {
@@ -7,24 +7,28 @@ const NavigationBar = () => {
     const handleToggle = () => {
         setIsOpen((isOpen) => !isOpen);
     }
+    
+    const location = useLocation();
 
     return (
-        <Navbar dark color="danger" container="lg" expand="sm" className="sticky-top">
+        <Navbar dark color="danger" container="lg" expand="sm" className="sticky-top shadow-lg p-3">
             <NavbarBrand className="logoBlock">
-                <img src="github-mark/github-mark-white.png" alt="logo" className="logo" />
-                <span className="logo-title">Github Finder</span>
+                <Link to="/" className="text-decoration-none text-light d-flex">
+                    <img src="github-mark/github-mark-white.png" alt="logo" className="logo" />
+                    <span className="logo-title">Github Finder</span>
+                </Link>
             </NavbarBrand>
             <NavbarToggler onClick={handleToggle} />
             <Collapse isOpen={isOpen} navbar>
                 <Nav navbar className="ms-auto">
                     <NavItem className="ms-2">
-                        <Link to="/" className="nav-link active">
-                                Home
+                        <Link to="/" className={`nav-link ${location.pathname === "/" ? "active" : ""}`}>
+                            Home
                         </Link>
                     </NavItem>
                     <NavItem className="ms-2">
-                        <Link to="/about" className="nav-link active">
-                                About
+                        <Link to="/about" className={`nav-link ${location.pathname === "/about" ? "active" : ""}`}>
+                            About
                         </Link>
                     </NavItem>
                 </Nav>
